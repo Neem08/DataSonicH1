ad7963ebe6e6d17c89d877d4c19845c2ac3b1e4e
Neem08 <99397614+Neem08@users.noreply.github.com>
2023-04-30 09:32:58 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Miniproject</title>
+    <title>WasteLess</title>
   </head>
   <body  style="background-color: #a6d5cb;" >
   <nav class="navbar navbar-light" style=" background-color: #16a083 ;">
